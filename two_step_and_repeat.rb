29544fd7d7935c197abe6fae4d6d1d1ffcd99d49
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-24 19:52:11 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -17,8 +17,7 @@ first_steps
 def a_few_more_steps
   # Write a loop that outputs the first two sets of steps in the Two-Step
   loop do 
-    puts "Turn"
-    sleep(0.5)
+   
     puts "Right foot back"
     sleep(0.5)
     puts "Left foot back"
@@ -40,7 +39,8 @@ a_few_more_steps
 
 def how_many_steps?
   steps =0
-  while steps 
+  while steps <= 30
+  
 end
 
 def break_dance
