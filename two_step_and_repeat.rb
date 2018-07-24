25e7faeeeaba07c17f1a819f0347f85af9b6ecfb
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-24 20:44:18 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,57 +1,57 @@
-# def first_steps
-#   loop do 
-#     puts "Right foot back"
-#     sleep(0.5)
-#     puts "Left foot back"
-#     sleep(0.5)
-#     puts "Right foot back"
-#     sleep(0.5)
-#     puts "Stop"
-#     sleep(1)
-#     break
-#   end
-# end 
+def first_steps
+  loop do 
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Left foot back"
+    sleep(0.5)
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Stop"
+    sleep(1)
+    break
+  end
+end 
 
-# first_steps
+first_steps
 
-# def a_few_more_steps
-#   # Write a loop that outputs the first two sets of steps in the Two-Step
-#   loop do 
+def a_few_more_steps
+  # Write a loop that outputs the first two sets of steps in the Two-Step
+  loop do 
    
-#     puts "Right foot back"
-#     sleep(0.5)
-#     puts "Left foot back"
-#     sleep(0.5)
-#     puts "Right foot back"
-#     sleep(0.5)
-#     puts "Right foot steps right and back"
-#     sleep(0.5)
-#     puts "Left foot crosses over right"
-#     sleep(0.5)
-#     puts "Right foot steps right"
-#     puts "Stop"
-#     sleep(1)
-#     break
-#   end
-# end
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Left foot back"
+    sleep(0.5)
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Right foot steps right and back"
+    sleep(0.5)
+    puts "Left foot crosses over right"
+    sleep(0.5)
+    puts "Right foot steps right"
+    puts "Stop"
+    sleep(1)
+    break
+  end
+end
 
-# a_few_more_steps
+a_few_more_steps
 
-# # def how_many_steps?
-#   # steps =0
-#   loop do 
-#   steps += 1
-#   puts steps
-#   if steps % 2 ==0 
-#     puts "left"
-#   else 
-#     puts "right"
-# end 
-#   sleep (0.5)
-# end
-# end
+# def how_many_steps?
+  # steps =0
+  loop do 
+  steps += 1
+  puts steps
+  if steps % 2 ==0 
+    puts "left"
+  else 
+    puts "right"
+end 
+  sleep (0.5)
+end
+end
 
-# how_many_steps
+how_many_steps
 
 def break_dance
   steps=0
